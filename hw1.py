0b4e3046c92bcbdaa47c682a1fb562e7d3bbb3cc
Oksanka-wd <oxana.fedotowa@gmail.com>
2026-07-26 22:57:08 +0300

    Refactor get_days_from_today function
    Refactor get_days_from_today function to include parameter and return type annotations. Update error handling and add usage examples.

--- hw1.py
+++ hw1.py
@@ -1,17 +1,21 @@
-
 from datetime import datetime
+def get_days_from_today(date: str)-> int:
+    """
+    Обчислює кількість днів між заданою датою та поточною датою.
 
-def get_days_from_today(date): 
-    try: 
-    # We convert the date string in the format 'YYYY-MM-DD' into a datetime object 
-    date_obj = datetime.strptime(date, "%Y-%m-%d") 
-    # We get the current date 
-    current_date = datetime.today() 
-    # We calculate the difference between the current date and the specified date
-    difference = current_date - date_obj 
-    # Return the difference in days as an integer
-     return difference.days
-    except ValueError: 
-        return "Invalid date format. Enter date in 'YYYY-MM-DD' format."
+    :param date: рядок в форматі 'YYYY-MM-DD'
+    :return: Ціле число - різниця в днях (може бути від'ємним) 
+    :raises ValueError: Якщо формат дати неправильний
+    """
 
-print(get_days_from_today("1984-06-06"))
+    try:
+        target_date = datetime.strptime(date, "%Y-%m-%d").date
+        today = datetime.today().date()
+        delta = today - target_date
+        raises delta.days
+
+    except ValueError:
+        raises "невірний формат дати. Використовуйте YYYY-MM-DD."
+
+        #приклади використання
+        print(get_days_from_today("2026-07-20")) #якщо сьогодні 2026-07-26
